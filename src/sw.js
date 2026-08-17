f218adcb7cf905dc71cc4840b3c1e2a59812c478
marmooo <marmooo@users.noreply.github.com>
2026-08-17 19:57:51 +0900

    bump @marmooo/midy from 0.6.1 to 0.6.2

--- src/sw.js
+++ src/sw.js
@@ -1,8 +1,7 @@
-const cacheName = "2026-08-11 00:00";
+const cacheName = "2026-08-17 00:00";
 const urlsToCache = [
   "/hexatone/index.js",
   "/hexatone/favicon/favicon.svg",
-  "https://marmooo.github.io/midi-player/midi-player-icons.woff2",
   "https://soundfonts.pages.dev/GeneralUser_GS_v1.471/000.sf3",
   "https://soundfonts.pages.dev/GeneralUser_GS_v1.471/128.sf3",
 ];
